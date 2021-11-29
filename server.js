e353bfe33825c6150432d6b835fe68c73f215c0f
evebigaj <eve.bigaj@gmail.com>
2021-11-29 11:45:35 -0500

    remove login

--- server.js
+++ server.js
@@ -2,20 +2,14 @@ const express = require('express')
 const app = express();
 const {paintings} = require('./routes/paintings')
 const {register} = require('./routes/register')
-const {login} = require('./config/login-logic')
-const passport = require('passport')
+
+
 
 app.use(express.urlencoded({ extended: true }))
 // // parse json
 app.use(express.json())
 
-passport.use()
 
-
-app.use(passport.initialize())
-
-
-app.use('/login', login)
 
 app.use('/paintings', paintings)
 app.use('/register', register)
