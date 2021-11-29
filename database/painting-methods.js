082b13b994f4471682d90cded909223024368668
evebigaj <eve.bigaj@gmail.com>
2021-11-29 12:17:47 -0500

    basic painting search

--- database/painting-methods.js
+++ database/painting-methods.js
@@ -26,17 +26,34 @@ const getPaintingById = async (id) => {
     const painting = await client.connect()
     .then(() => client.query(`select * from paintings where id = ${id}`))
     .then(result => result.rows)
-    .catch(error => {console.log(`oops: ${error}`)})
+    .catch(error => {console.log(`oops: ${error}`)
+return error})
     .finally(result => {
-        client.end();
-        return result
+        client.end()
     })
     return painting
 }
+
+const getPaintingByKey = async (key, value) => {
+    const result = await client.connect()
+    .then(() =>{console.log('connected')})
+    .then(() => client.query(`select * from paintings where ${key} = '${value}'`))
+    .then(result => {
+        console.table(result.rows)
+        return result.rows
+    })
+    .catch(error => {console.log(`oops: ${error}`)
+return error})
+.finally(()=>{client.end()})
+
+return result 
+}
+
+
 //this is a promise because getPaintings is async
 //console.log(`the result is ${getPaintings()}`)
 
-module.exports = {getPaintings, getPaintingById}
+module.exports = {getPaintings, getPaintingById, getPaintingByKey}
 
 
 
--- routes/paintings.js
+++ routes/paintings.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const paintings = new express.Router()
-const {getPaintings, getPaintingById} = require('../database/painting-methods')
+const {getPaintings, getPaintingById, getPaintingByKey} = require('../database/painting-methods')
 
 paintings.get('/', (req, res)=>{
     console.log('getting');
+    if(!req.query){
     getPaintings()
     .then(result => {
         if(!result){
             res.status(404).send('Resource not found')
         }
         console.log(result);
+        res.send(result)})}
+    else{
+        //for now, assuming single element query
+        //for now, assuming we're searching specifically by medium
+        //todo: refactor so there isn't duplicate code
+        getPaintingByKey('medium', req.query.medium)
+        .then(result => {
+        if(!result){
+            res.status(404).send('Resource not found')
+        }
+        if(Object.keys(result).length ===0){
+            res.status(204).send()
+        }
+        console.log(result);
         res.send(result)})
+    }
 })
 
 paintings.get('/:id', (req, res) => {
