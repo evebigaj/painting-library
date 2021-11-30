c84cc649f74d302cb3a38595655f3d4c2da53d9c
evebigaj <eve.bigaj@gmail.com>
2021-11-30 14:12:07 -0500

    refactor getPaintings and getPaintingsByKeys

--- routes/paintings.js
+++ routes/paintings.js
@@ -3,19 +3,6 @@ const paintings = new express.Router()
 const {getPaintings, getPaintingById, getPaintingByKey, getPaintingsByKeys} = require('../database/painting-methods')
 
 paintings.get('/', (req, res)=>{
-    console.log('getting');
-    if(!req.query){
-    getPaintings()
-    .then(result => {
-        if(!result){
-            res.status(404).send('Resource not found')
-        }
-        console.log(result);
-        res.send(result)})}
-    else{
-        //for now, assuming single element query
-        //for now, assuming we're searching specifically by medium
-        //todo: refactor so there isn't duplicate code
         getPaintingsByKeys(req.query)
         .then(result => {
         if(!result){
@@ -28,7 +15,7 @@ paintings.get('/', (req, res)=>{
         res.send(result)})
         .catch(e => {console.log(e)})
     }
-})
+)
 
 paintings.get('/:id', (req, res) => {
     getPaintingById(req.params.id)
