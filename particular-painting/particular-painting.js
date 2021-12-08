f04819d0d9e8d456538730a0bfb6c9c8650d137c
evebigaj <eve.bigaj@gmail.com>
2021-12-08 18:05:08 -0500

    style particular-paintings

--- particular-painting/particular-painting.js
+++ particular-painting/particular-painting.js
@@ -8,18 +8,19 @@ fetch(`/api/paintings/${id}`)
 .then(result => result.json())
 .then(result => {
     let painting = result[0]
-    let info = document.getElementById("info")
+    let imageBox = document.getElementById("image-box")
+    let description = document.getElementById('description')
     console.log(painting.url)
     let image = document.createElement('img')
     image.src = painting.url;
-    info.append(image)
+    imageBox.append(image)
     let heading = document.createElement('h1')
     heading.innerHTML = painting.title;
-    info.append(heading);
+    description.append(heading);
     let dimensions = document.createElement('p');
     dimensions.innerHTML = `${painting.width}"x${painting.height}"`
-    info.append(dimensions)
+    description.append(dimensions)
     let price = document.createElement('p');
     price.innerHTML = `$${painting.price}`
-    info.append(price)
+    description.append(price)
 })
