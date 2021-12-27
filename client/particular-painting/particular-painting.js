e0667ae890873e26e271faf365101da27ab6d7a1
evebigaj <eve.bigaj@gmail.com>
2021-12-27 12:51:08 -0500

    clean up particular-paintings

--- client/particular-painting/particular-painting.js
+++ client/particular-painting/particular-painting.js
@@ -1,4 +1,4 @@
-//finding the id of the painting to fetch from the url: 
+//1) finding the id of the painting to fetch from the url: 
 let url = window.location.href
 //finds first number immediately after backslash
 //(since there may be numbers in the url)
@@ -10,7 +10,7 @@ let id = url.slice(numStringsToCut,url.length-1)
 let cartActionIcon = document.getElementById('cartActionIcon');
 let cartActionDescription = document.getElementById("cartActionDescription")
 
-
+//2) finding/setting session_id
 // Sets session_id to be either what is in local storage
 //or maximum session_id in 'carts' database + 1. 
 
@@ -22,41 +22,19 @@ let cartActionDescription = document.getElementById("cartActionDescription")
 //this is ~fine given current small number of users 
 //but should be fixed in final version
 
-
-
 const createStorageId = async () => {
     result = await fetch('/api/cart/session')
-.then(result => result.json())
-.then(result => {console.log(result) 
-    return result.max +1})
-.then(newId => {sessionStorage.setItem('session_id', newId)
-})
+    .then(result => result.json())
+    .then(result => result.max +1)
+    .then(newId => {sessionStorage.setItem('session_id', newId)
+    })
 return result 
 }
 
 const session = sessionStorage.getItem('session_id') || createStorageId()
-console.log(`the session id is ${session}`)
 
-fetch(`/api/cart/${id}?session=${session}`)
-.then(result => {
-    
-    if(result.status === 200){
-    
-    return true
-}
-else 
-{
-    return false}
-})
-.then(isInCart => { 
-cartActionIcon.src = isInCart? "/photos/minus.png":"/photos/plus.png"
-return isInCart
-} )
-.then(isInCart => {cartActionDescription.innerHTML = 
-isInCart? 'Remove from basket': 'Add to basket'})
+//3) Fetch and display painting and description
 
-
-console.log(`fetching the painting`)
 fetch(`/api/paintings/${id}`)
 .then(result => result.json())
 .then(result => {
@@ -78,49 +56,55 @@ fetch(`/api/paintings/${id}`)
     description.append(price)
 })
 
+//4) Add and remove from cart logic 
 
-
-
-const addToCart = async () => {
-//console.log(`/api/cart/${id}?session=${session}`)
-let isInCart = await fetch(`/api/cart/${id}?session=${session}`)
+//choose whether to display 'add' or 'remove' to/from cart 
+fetch(`/api/cart/${id}?session=${session}`)
 .then(result => {
-    console.log(result.status)
-
-    if(result.status === 200){
-
+    if(result.status === 200){  
     return true
-}
-else 
-{
+    }
+else {
     return false}
 })
+.then(isInCart => { 
+cartActionIcon.src = isInCart? "/photos/minus.png":"/photos/plus.png"
+return isInCart
+} )
+.then(isInCart => {cartActionDescription.innerHTML = 
+isInCart? 'Remove from basket': 'Add to basket'})
 
-console.log(`Is in cart is ${isInCart}`)
+
+
+//onclick behavior: 
+const addToCart = async () => {
+    let isInCart = await fetch(`/api/cart/${id}?session=${session}`)
+    .then(result => {
+    if(result.status === 200){
+        return true
+    }
+    else {
+        return false
+        }
+    })
+
     
-    
+    //the case where we're adding to cart:
    if(!isInCart){
-       console.log('about to send to cart')
+       //add to cart:
        await fetch(`/api/cart/${id}?session=${session}`, {method: 'POST'})
-   
-
-   cartActionIcon.src = '/photos/minus.png'
-   
-   cartActionDescription.innerHTML = "Remove from basket"
-   
+       //change icon and description to 'remmove' 
+       cartActionIcon.src = '/photos/minus.png'
+        cartActionDescription.innerHTML = "Remove from basket"
    }
-
-   else if(isInCart){console.log('is in cart')
+   //case where we're removing from cart
+   else if(isInCart){
+       //remove from cart:
        await fetch(`/api/cart/${id}?session=${sessionStorage.getItem('session_id')}`, {method: 'DELETE'})
+       //chnage icon and description to "add"
        cartActionIcon.src = '/photos/plus.png'
-    cartActionDescription.innerHTML = "Add to basket"
+        cartActionDescription.innerHTML = "Add to basket"
 
    }
-   //make remove from basket appear on load
-   //make the button action for when it's remove
-   //(i.e. delete route)
-
-   //make the state depend on the state of the cart rather than defaulting
-   //change text
-   //change state so that clicking now removes from cart 
+   
 }
