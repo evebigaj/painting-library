5f3542d7514d03ebdfb40ac117f645c8282419a2
evebigaj <eve.bigaj@gmail.com>
2021-12-27 14:39:01 -0500

    cleanup routes/submit

--- routes/submit.js
+++ routes/submit.js
@@ -4,16 +4,9 @@ const submit = new Router()
 
 
 submit.post('/', (req, res)=>{
-    console.log('we hit the backend submission')
-    console.log(`the request body is ${req.body} and its content is ${req.body.content}`)
-sendEmail(req.body.content, res)
-res.send('success')
-})
+    sendEmail(req.body.content, res)
+    res.send('success')
+    })
 
 module.exports = {submit}
 
-//next step: lessons
-// send the form info in req.body.contents 
-
-//next step: onsubmit, run a function which will post the content 
-//and also which will make the paintings unavailable
