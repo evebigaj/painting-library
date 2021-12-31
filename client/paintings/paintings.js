701b85f8a67b1355dacce2b98b9fa69477ac0c3d
evebigaj <eve.bigaj@gmail.com>
2021-12-31 13:54:25 -0500

    style paintings

--- client/paintings/paintings.js
+++ client/paintings/paintings.js
@@ -9,9 +9,10 @@ fetch(`/api/paintings?available=true&${queryString}`)
 .then(result => {
     return result.json()})
 .then(result =>{
-    let container = document.getElementById("container");
+    let container = document.getElementById("painting-container");
     result.forEach(painting => {
         let image = document.createElement('img');
+        image.classList.add('painting-image')
         image.src = painting.url;
         image.onclick = () => {location.href = painting.id}
         container.append(image)
